de8fe3f1af7d0b5452586bcd06b18ecfce532dbd
dominik-private's <dominikkalinowski15@wp.pl>
2021-12-26 11:30:23 +0100

    init: repository created
    angular 12 project created,
    material installed

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'writting-app';
+
+  panelOpenState = false;
+
+}
