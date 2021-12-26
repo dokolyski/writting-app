08387496b1a68a4f91c8b2a1ce897740c9309df1
dominik-private's <dominikkalinowski15@wp.pl>
2021-12-26 13:54:45 +0100

    feat: quill editor added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,32 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'writting-app';
 
   panelOpenState = false;
 
+  constructor() {}
+
+  onSelectionChanged(event: any): void {
+    if (event.oldRange == null) {
+      this.onFocus();
+    }
+    if (event.range == null) {
+      this.onBlur();
+    }
+  }
+
+  onContentChanged(event: any): void {
+    console.log(event.html);
+  }
+
+  onFocus(): void {
+    console.log('On Focus');
+  }
+  onBlur(): void {
+    console.log('Blurred');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatSliderModule} from "@angular/material/slider";
-import {MatExpansionModule} from "@angular/material/expansion";
+import { MatSliderModule } from '@angular/material/slider';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { QuillModule } from 'ngx-quill';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSliderModule,
-    MatExpansionModule
+    MatExpansionModule,
+    QuillModule.forRoot(),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
